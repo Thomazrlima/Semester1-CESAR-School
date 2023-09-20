b1aebd7ce19bca78f841b962211eb186917dcef3
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-20 11:48:06 -0300

    Create main-spec.js

---
+++ Testes_HTML/main-spec.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+var numposts = 6;
+var showpostthumbnails = true;
+var displaymore = false;
+var displayseparator = false;
+var showcommentnum = false;
+var showpostdate = true;
+var showpostsummary = false;
+var showstyleblog = false;
+var numchars = 200;
+</script>
+<script type="text/javascript" src="/feeds/posts/default/-/Ore%20Monogatari?orderby=updated&
+alt=json-in-script&callback=labelthumbs"></script>
+<iframe width="690" height="400" src="https://www.youtube.com/embed/BbzalAgbahc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
